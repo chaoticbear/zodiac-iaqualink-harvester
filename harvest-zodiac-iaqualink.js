0e26f0fa2814e875838ce3a611cff18a2a0eaf3e
Chris Studer <christopher.studer@gmail.com>
2019-07-30 19:57:14 -0500

    Added open weather map for login temp from a weather station nearby.

--- harvest-zodiac-iaqualink.js
+++ harvest-zodiac-iaqualink.js
@@ -1,12 +1,16 @@
-//Update the settings and node run this file.
-const userID = 'yourUserEmail@gmail.com';
+const userID = 'yourEmailUser';
 const userPassword = 'yourPassword';
 const pageUrl = 'https://iaqualink.zodiacpoolsystems.com/';
 const outputFileName = './pool-temp.txt';
+const openWeatherMapAPIKey = 'YourLongApiId';
+// Can select a city Id on a Dallas TX one right now.
+// https://openweathermap.org/find?q=
+const openWeatherCityId = '4684888';
 
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
+const weather = require('openweather-apis');
 (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -30,7 +34,6 @@ const path = require('path');
         element.click();
       }
     });
-
     // Location takes a while to init and load page lets just wait awhile
     await page.waitFor(15000);
     //console.log('Tabs ', (await browser.pages()).length);
@@ -38,19 +41,48 @@ const path = require('path');
     // Wait for the page to load the data point divs
     await tabThree.waitForSelector('.home_info_value');
     // Get Pool Temp
-    const poolTxt = await tabThree.evaluate(() => document.querySelector("[id='1_25_0']").innerText);
+    let poolTxt = await tabThree.evaluate(() => document.querySelector("[id='1_25_0']").innerText);
+    if (poolTxt != '--') {
+      poolTxt = poolTxt.replace(/\D/g,'');
+    }
     console.log('Pool Temp ', poolTxt);
     // Get Air Temp
-    const airTxt = await tabThree.evaluate(() => document.querySelector("[id='1_25_1']").innerText);
+    let airTxt = await tabThree.evaluate(() => document.querySelector("[id='1_25_1']").innerText);
+    if (airTxt != '--') {
+      airTxt = airTxt.replace(/\D/g,'');
+    }
     console.log('Air Temp ', airTxt);
-    // Get the Y-md H:i:s Date
-    let today = new Date()
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    let filePath = path.resolve(outputFileName);
-    await fs.appendFile(filePath, (date + ' ' + time) + ',' + poolTxt + ',' + airTxt + '\n', function (err) {
-      if (err) throw err;
-      console.log('Saved to ' + filePath);
-    });
     browser.close();
+    //Hard part is done lets get current weather data from open weather api
+    await weather.setLang('en');
+    await weather.setCityId(openWeatherCityId);
+    await weather.setAPPID(openWeatherMapAPIKey);
+    let tempTxt = ''
+    let humidTxt = ''
+    // Get the current Temperature
+    await weather.getTemperature(function (err, temp) {
+      if (err) console.log('Err: ', err);
+      tempTxt = temp;
+      // Convert to F
+      tempTxt = (tempTxt * 9/5) + 32;
+      tempTxt = tempTxt.toFixed(2);
+      console.log('Current Temp ', tempTxt);
+      // Get the current Humidity
+      weather.getHumidity(function (err, humid) {
+        if (err) console.log('Err: ', err);
+        humidTxt = humid;
+        console.log('Current Humidity ', humidTxt);
+        // Get the Y-md H:i:s Date
+        let today = new Date()
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        let dateTime = date + ' ' + time
+        // Write to file
+        let filePath = path.resolve(outputFileName);
+        fs.appendFile(filePath, dateTime + ',' + poolTxt + ',' + airTxt + ',' + tempTxt + ',' + humidTxt + '\n', function (err) {
+          if (err) throw err;
+          console.log('Saved to ' + filePath);
+        });
+      });
+    });
 })();
